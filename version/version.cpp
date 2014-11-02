781f23df1f129ceeea5bafad372b4e388756789c
Hugh Sorby <h.sorby@auckland.ac.nz>
2014-11-03 09:31:45 +1300

    Correct failing test and add new test for library version string.

--- version/version.cpp
+++ version/version.cpp
@@ -3,7 +3,10 @@
 #include "yawot/version.h"
 
 TEST(Version, version_match) {
-    unsigned int version = yawot::getVersion();
+    unsigned int version = yawot::version();
     EXPECT_EQ(0x000100, version);
+
+    std::string version_string = yawot::versionString();
+    EXPECT_EQ("0.1.0", version_string);
 }
 
